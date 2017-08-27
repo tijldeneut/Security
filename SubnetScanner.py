99b5994ede2f94cfb39e8896471c7224f13b2e7f
Tijl Deneut <deneut_tijl@hotmail.com>
2017-08-27 22:15:42 +0200

    Update and rename subnet-scanner.py to SubnetScanner.py

--- SubnetScanner.py
+++ SubnetScanner.py
@@ -15,7 +15,7 @@
         You should have received a copy of the GNU General Public License
         along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-        File name subnet-scanner.py
+        File name SubnetScanner.py
         written by tijl[dot]deneut[at]howest[dot]be
 
         Written for Python2.x
