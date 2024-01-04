4fbbe41ad3833bf3f4bee79c14d1dd6ef79eb64f
Tijl Deneut <deneut_tijl@hotmail.com>
2024-01-04 15:38:19 +0100

    Update SSH-fingerprinter.py

--- SSH-fingerprinter.py
+++ SSH-fingerprinter.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*- 
 r''' 
-    	Copyright 2023 Photubias(c)
+    	Copyright 2024 Photubias(c)
 
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
